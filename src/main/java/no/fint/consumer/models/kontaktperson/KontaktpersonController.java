60de86feebb1ce7a80d99c18e00b9e42e84d142c
Asgeir Nilsen <asgeir@fintprosjektet.no>
2019-02-05 12:01:21 +0100

    Added update mappings for kontaktperson.

--- src/main/java/no/fint/consumer/models/kontaktperson/KontaktpersonController.java
+++ src/main/java/no/fint/consumer/models/kontaktperson/KontaktpersonController.java
@@ -149,6 +149,102 @@ public class KontaktpersonController {
         return kontaktperson.map(linker::toResource).orElseThrow(() -> new EntityNotFoundException(id));
     }
 
+
+
+    @GetMapping("/status/{id}")
+    public ResponseEntity getStatus(
+            @PathVariable String id,
+            @RequestHeader(HeaderConstants.ORG_ID) String orgId,
+            @RequestHeader(HeaderConstants.CLIENT) String client) {
+        log.debug("/status/{} for {} from {}", id, orgId, client);
+        if (!statusCache.containsKey(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        Event event = statusCache.get(id);
+        log.debug("Event: {}", event);
+        log.trace("Data: {}", event.getData());
+        if (!event.getOrgId().equals(orgId)) {
+            return ResponseEntity.badRequest().body(new EventResponse() { { setMessage("Invalid OrgId"); } } );
+        }
+        if (event.getResponseStatus() == null) {
+            return ResponseEntity.status(HttpStatus.ACCEPTED).build();
+        }
+        List<KontaktpersonResource> result = objectMapper.convertValue(event.getData(), objectMapper.getTypeFactory().constructCollectionType(List.class, KontaktpersonResource.class));
+        switch (event.getResponseStatus()) {
+            case ACCEPTED:
+                if (event.getOperation() == Operation.VALIDATE) {
+                    fintAuditService.audit(event, Status.SENT_TO_CLIENT);
+                    return ResponseEntity.ok(event.getResponse());
+                }
+                URI location = UriComponentsBuilder.fromUriString(linker.getSelfHref(result.get(0))).build().toUri();
+                event.setMessage(location.toString());
+                fintAuditService.audit(event, Status.SENT_TO_CLIENT);
+                return ResponseEntity.status(HttpStatus.SEE_OTHER).location(location).build();
+            case ERROR:
+                fintAuditService.audit(event, Status.SENT_TO_CLIENT);
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(event.getResponse());
+            case CONFLICT:
+                fintAuditService.audit(event, Status.SENT_TO_CLIENT);
+                return ResponseEntity.status(HttpStatus.CONFLICT).body(linker.toResources(result));
+            case REJECTED:
+                fintAuditService.audit(event, Status.SENT_TO_CLIENT);
+                return ResponseEntity.badRequest().body(event.getResponse());
+        }
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(event.getResponse());
+    }
+
+    @PostMapping
+    public ResponseEntity postKontaktperson(
+            @RequestHeader(name = HeaderConstants.ORG_ID) String orgId,
+            @RequestHeader(name = HeaderConstants.CLIENT) String client,
+            @RequestBody KontaktpersonResource body,
+            @RequestParam(name = "validate", required = false) boolean validate
+    ) {
+        log.debug("postKontaktperson, Validate: {}, OrgId: {}, Client: {}", validate, orgId, client);
+        log.trace("Body: {}", body);
+        linker.mapLinks(body);
+        Event event = new Event(orgId, Constants.COMPONENT, FellesActions.UPDATE_KONTAKTPERSON, client);
+        event.addObject(objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS).convertValue(body, Map.class));
+        event.setOperation(Operation.CREATE);
+        if (validate) {
+            event.setQuery("VALIDATE");
+            event.setOperation(Operation.VALIDATE);
+        }
+        fintAuditService.audit(event);
+
+        consumerEventUtil.send(event);
+
+        statusCache.put(event.getCorrId(), event);
+
+        URI location = UriComponentsBuilder.fromUriString(linker.self()).path("status/{id}").buildAndExpand(event.getCorrId()).toUri();
+        return ResponseEntity.status(HttpStatus.ACCEPTED).location(location).build();
+    }
+
+  
+    @PutMapping("/systemid/{id:.+}")
+    public ResponseEntity putKontaktpersonBySystemId(
+            @PathVariable String id,
+            @RequestHeader(name = HeaderConstants.ORG_ID) String orgId,
+            @RequestHeader(name = HeaderConstants.CLIENT) String client,
+            @RequestBody KontaktpersonResource body
+    ) {
+        log.debug("putKontaktpersonBySystemId {}, OrgId: {}, Client: {}", id, orgId, client);
+        log.trace("Body: {}", body);
+        linker.mapLinks(body);
+        Event event = new Event(orgId, Constants.COMPONENT, FellesActions.UPDATE_KONTAKTPERSON, client);
+        event.setQuery("systemid/" + id);
+        event.addObject(objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS).convertValue(body, Map.class));
+        event.setOperation(Operation.UPDATE);
+        fintAuditService.audit(event);
+
+        consumerEventUtil.send(event);
+
+        statusCache.put(event.getCorrId(), event);
+
+        URI location = UriComponentsBuilder.fromUriString(linker.self()).path("status/{id}").buildAndExpand(event.getCorrId()).toUri();
+        return ResponseEntity.status(HttpStatus.ACCEPTED).location(location).build();
+    }
+
     @DeleteMapping("/systemid/{id:.+}")
     public ResponseEntity deleteKontaktpersonBySystemId(
             @PathVariable String id,
