e40455008124362f979d61cb59440e85f7d0896a
Asgeir Storesund Nilsen <asgeir@fintlabs.no>
2020-04-23 08:32:24 +0200

    Version 3.4.5. (#11)
    * Version 3.4.5.
    
    * Updated specs.
    
    * Remove k8s manifests, ref FINTLabs/fint-kubernetes#2

--- src/main/java/no/fint/consumer/config/Constants.java
+++ src/main/java/no/fint/consumer/config/Constants.java
@@ -12,28 +12,28 @@ public enum Constants {
     public static final String CACHE_INITIALDELAY_BASISGRUPPE = "${fint.consumer.cache.initialDelay.basisgruppe:900000}";
     public static final String CACHE_FIXEDRATE_BASISGRUPPE = "${fint.consumer.cache.fixedRate.basisgruppe:900000}";
     
-    public static final String CACHE_INITIALDELAY_ELEV = "${fint.consumer.cache.initialDelay.elev:910000}";
+    public static final String CACHE_INITIALDELAY_ELEV = "${fint.consumer.cache.initialDelay.elev:960000}";
     public static final String CACHE_FIXEDRATE_ELEV = "${fint.consumer.cache.fixedRate.elev:900000}";
     
-    public static final String CACHE_INITIALDELAY_ELEVFORHOLD = "${fint.consumer.cache.initialDelay.elevforhold:920000}";
+    public static final String CACHE_INITIALDELAY_ELEVFORHOLD = "${fint.consumer.cache.initialDelay.elevforhold:1020000}";
     public static final String CACHE_FIXEDRATE_ELEVFORHOLD = "${fint.consumer.cache.fixedRate.elevforhold:900000}";
     
-    public static final String CACHE_INITIALDELAY_KONTAKTLARERGRUPPE = "${fint.consumer.cache.initialDelay.kontaktlarergruppe:930000}";
+    public static final String CACHE_INITIALDELAY_KONTAKTLARERGRUPPE = "${fint.consumer.cache.initialDelay.kontaktlarergruppe:1080000}";
     public static final String CACHE_FIXEDRATE_KONTAKTLARERGRUPPE = "${fint.consumer.cache.fixedRate.kontaktlarergruppe:900000}";
     
-    public static final String CACHE_INITIALDELAY_KONTAKTPERSON = "${fint.consumer.cache.initialDelay.kontaktperson:940000}";
+    public static final String CACHE_INITIALDELAY_KONTAKTPERSON = "${fint.consumer.cache.initialDelay.kontaktperson:1140000}";
     public static final String CACHE_FIXEDRATE_KONTAKTPERSON = "${fint.consumer.cache.fixedRate.kontaktperson:900000}";
     
-    public static final String CACHE_INITIALDELAY_MEDLEMSKAP = "${fint.consumer.cache.initialDelay.medlemskap:950000}";
+    public static final String CACHE_INITIALDELAY_MEDLEMSKAP = "${fint.consumer.cache.initialDelay.medlemskap:1200000}";
     public static final String CACHE_FIXEDRATE_MEDLEMSKAP = "${fint.consumer.cache.fixedRate.medlemskap:900000}";
     
-    public static final String CACHE_INITIALDELAY_PERSON = "${fint.consumer.cache.initialDelay.person:960000}";
+    public static final String CACHE_INITIALDELAY_PERSON = "${fint.consumer.cache.initialDelay.person:1260000}";
     public static final String CACHE_FIXEDRATE_PERSON = "${fint.consumer.cache.fixedRate.person:900000}";
     
-    public static final String CACHE_INITIALDELAY_SKOLERESSURS = "${fint.consumer.cache.initialDelay.skoleressurs:970000}";
+    public static final String CACHE_INITIALDELAY_SKOLERESSURS = "${fint.consumer.cache.initialDelay.skoleressurs:1320000}";
     public static final String CACHE_FIXEDRATE_SKOLERESSURS = "${fint.consumer.cache.fixedRate.skoleressurs:900000}";
     
-    public static final String CACHE_INITIALDELAY_UNDERVISNINGSFORHOLD = "${fint.consumer.cache.initialDelay.undervisningsforhold:980000}";
+    public static final String CACHE_INITIALDELAY_UNDERVISNINGSFORHOLD = "${fint.consumer.cache.initialDelay.undervisningsforhold:1380000}";
     public static final String CACHE_FIXEDRATE_UNDERVISNINGSFORHOLD = "${fint.consumer.cache.fixedRate.undervisningsforhold:900000}";
     
 
--- src/main/java/no/fint/consumer/models/basisgruppe/BasisgruppeLinker.java
+++ src/main/java/no/fint/consumer/models/basisgruppe/BasisgruppeLinker.java
@@ -8,10 +8,10 @@ import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static java.util.Objects.isNull;
 import static org.springframework.util.StringUtils.isEmpty;
-
 
 @Component
 public class BasisgruppeLinker extends FintLinker<BasisgruppeResource> {
@@ -34,11 +34,17 @@ public class BasisgruppeLinker extends FintLinker<BasisgruppeResource> {
 
     @Override
     public String getSelfHref(BasisgruppeResource basisgruppe) {
+        return getAllSelfHrefs(basisgruppe).findFirst().orElse(null);
+    }
+
+    @Override
+    public Stream<String> getAllSelfHrefs(BasisgruppeResource basisgruppe) {
+        Stream.Builder<String> builder = Stream.builder();
         if (!isNull(basisgruppe.getSystemId()) && !isEmpty(basisgruppe.getSystemId().getIdentifikatorverdi())) {
-            return createHrefWithId(basisgruppe.getSystemId().getIdentifikatorverdi(), "systemid");
+            builder.add(createHrefWithId(basisgruppe.getSystemId().getIdentifikatorverdi(), "systemid"));
         }
         
-        return null;
+        return builder.build();
     }
 
     int[] hashCodes(BasisgruppeResource basisgruppe) {
