c1a26ce2ef969e3e1568f1106f1a2f4b9cc5bccc
Asgeir Nilsen <asgeir@fintlabs.no>
2021-01-07 14:58:06 +0100

    Remove NonNull.

--- src/main/java/no/fint/consumer/security/FintAccessPrincipal.java
+++ src/main/java/no/fint/consumer/security/FintAccessPrincipal.java
@@ -2,10 +2,9 @@ package no.fint.consumer.security;
 
 import lombok.Builder;
 import lombok.Data;
-import lombok.NonNull;
 
 @Data
 @Builder
 public final class FintAccessPrincipal {
-    private final @NonNull String name, orgId;
+    private final String name, orgId;
 }
