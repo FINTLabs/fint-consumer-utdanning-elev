d14a154b7e46f23a1572264e849e249e2e274471
Asgeir Nilsen <asgeir@fintlabs.no>
2021-03-09 10:56:38 +0100

    Updated access-policy and stateless session management.

--- src/main/java/no/fint/consumer/config/ApplicationSecurity.java
+++ src/main/java/no/fint/consumer/config/ApplicationSecurity.java
@@ -11,6 +11,7 @@ import org.springframework.security.access.vote.UnanimousBased;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationProvider;
 
 import java.util.Collections;
@@ -58,7 +59,7 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .sessionManagement().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .addFilter(fintRequestHeaderPreauthProcessingFilter())
                 .authenticationProvider(preAuthenticatedAuthenticationProvider())
                 .authorizeRequests()
