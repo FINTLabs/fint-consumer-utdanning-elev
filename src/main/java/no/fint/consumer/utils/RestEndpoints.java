43a98c420bc842d459cf320c3f03a5dbd72fb2bc
Nils-Odd Solberg <nilsodd@fintlabs.no>
2021-09-30 15:14:12 +0200

    Version 3.10.0-rc-1.

--- src/main/java/no/fint/consumer/utils/RestEndpoints.java
+++ src/main/java/no/fint/consumer/utils/RestEndpoints.java
@@ -4,10 +4,12 @@ public enum RestEndpoints {
     ;
 
     public static final String ADMIN = "/admin";
+	public static final String ANMERKNINGER = "/anmerkninger";
 	public static final String BASISGRUPPE = "/basisgruppe";
 	public static final String BASISGRUPPEMEDLEMSKAP = "/basisgruppemedlemskap";
 	public static final String ELEV = "/elev";
 	public static final String ELEVFORHOLD = "/elevforhold";
+	public static final String ELEVTILRETTELEGGING = "/elevtilrettelegging";
 	public static final String KONTAKTLARERGRUPPE = "/kontaktlarergruppe";
 	public static final String KONTAKTLARERGRUPPEMEDLEMSKAP = "/kontaktlarergruppemedlemskap";
 	public static final String KONTAKTPERSON = "/kontaktperson";
