4088f75cf873291c2b9cf54b3e04afe59cc895d5
Hknots <hknots99@gmail.com>
2024-05-21 13:10:20 +0200

    more

--- src/main/java/no/fint/consumer/models/medlemskap/MedlemskapController.java
+++ src/main/java/no/fint/consumer/models/medlemskap/MedlemskapController.java
@@ -182,7 +182,7 @@ public class MedlemskapController {
 
             fintAuditService.audit(response, Status.SENT_TO_CLIENT);
 
-            return linker.toResource(medlemskap);
+            return linker.mapAndResetLinks(medlemskap);
         }    
     }
 
